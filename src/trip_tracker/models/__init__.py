e0a7917ee0a77b88502075d60b19efbb3bffb293
owine <owine@users.noreply.github.com>
2026-04-29 07:01:10 -0500

    feat(models): add Trip and TripTraveler

--- src/trip_tracker/models/__init__.py
+++ src/trip_tracker/models/__init__.py
@@ -2,5 +2,7 @@
 
 from trip_tracker.models.forwarding_alias import ForwardingAlias  # noqa: F401
 from trip_tracker.models.raw_email import RawEmail  # noqa: F401
+from trip_tracker.models.trip import Trip  # noqa: F401
+from trip_tracker.models.trip_traveler import TripTraveler  # noqa: F401
 from trip_tracker.models.user import User  # noqa: F401
 from trip_tracker.models.webhook_replay import WebhookReplay  # noqa: F401
